16c03d6f4d9171750de6327eb95b94a87b91d38b
Aaron Rodi <metalhead5209@gmail.com>
2022-10-22 12:30:16 -0500

    implemented tests using test driven development

--- LinkedList.js
+++ LinkedList.js
@@ -18,5 +18,13 @@ class Node {
     }
 }
 
+LinkedList.fromValues = function(...values) {
+    const list = new LinkedList()
+    for (let i = values.length - 1; i >= 0; i--) {
+        list.insertAtHead(values[i])
+    }
+    return list
+}
+
 
 module.exports = LinkedList;
--- LinkedListTest.test.js
+++ LinkedListTest.test.js
@@ -12,3 +12,38 @@ describe('#insertAtHead', () => {
         expect(list.length).toBe(2)
     })
 })
+
+ describe('#getByIndex', () => {
+    describe('with index less than 0', () => {
+        test('it returns null', () => {
+            const list = LinkedList.fromValues(10, 20)
+
+            expect(list.getByIndex(-1)).toBeNull()
+        })
+    })
+    describe('with index 0', () => {
+        test('it returns the head', () => {
+            const list = LinkedList.fromValues(10,20)
+    
+            expect(list.getByIndex(0).value).toBe(10)
+        })
+     })
+    
+     describe('with index in the middle', () => {
+        test('it returns the element at the index', () => {
+            const list = LinkedList.fromValues(10,20,40,50,60)
+    
+            expect(list.getByIndex(3).value).toBe(50)
+        })
+     })
+     describe('with index in the middle', () => {
+        test('it returns the element at the index', () => {
+            const list = LinkedList.fromValues(10,20,40,50,60)
+    
+            expect(list.getByIndex(3).value).toBe(50)
+        })
+     })
+    
+ })
+
+ 
